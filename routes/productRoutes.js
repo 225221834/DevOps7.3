bfdf68daf68868ed709caf6a3e83636c013cbc40
5522551834 <s225221834@deakin.edu.au>
2025-09-25 13:34:40 +1000

    changes commits

--- routes/productRoutes.js
+++
@@ -1,8 +0,0 @@
- 
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-router.get('/products', productController.getProductsByCategory);
-
-module.exports = router;
